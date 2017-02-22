b493cdeeccc06bc4da9c96a8ca56188a520ade2a
cholq <cdholquist@gmail.com>
2017-02-22 17:44:41 -0500

    added logic to output csv data

--- src/nj_county_parser.py
+++ src/nj_county_parser.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import os.path
 import json
+import csv
 
 arg_parser = argparse.ArgumentParser(description='Parse New Jersey Count data.')
 arg_parser.add_argument('configfile', type=str, nargs=1)
@@ -24,13 +25,14 @@ def readJsonConfig ( p_args ):
 def openOutputFile( p_config ):
     full_output_file = os.path.join(p_config['output_directory'], p_config['output_file'])
     try:
-        f = open( full_output_file, 'w')
+        f = open( full_output_file, 'w') 
+        writer = csv.writer(f, quoting=csv.QUOTE_NONE)
     except:
         sys.exit('ERROR: Could not open output file: ' + full_output_file)
-    return f
+    return writer
 
 def print_header( p_outfile):
-    p_outfile.write('county,office,district,party,candidate,votes\n')
+    p_outfile.writerow( ('county','office','district','party','candidate','votes') )
     return
 
 def doesJsonKeyExist( p_config, p_key ):
@@ -43,31 +45,44 @@ def get_county_name(p_infile):
     return
 
 def clean_text_values(p_text):
-    clean_text = p_text.replace('"', '')
+    clean_text = p_text.replace(',', '')
     return clean_text
 
 def populate_candidate_party_lists( p_candidateList, p_partyList, p_header):
     counter = 0
     value0 = ""
     value1 = ""
-    for value in p_header.split('|'):
+    for value in p_header.split('\r'):
         if counter == 0:
-            value0 = clean_text_values(value)
+            value0 = value
         elif counter == 1:
-            value1 = clean_text_values(value)
+            value1 = value
         counter = counter + 1
     if counter > 1:
         p_candidateList.append(value0)
         p_partyList.append(value1)
     return
 
+def print_county_totals( p_candidateList, p_partyList, p_line, p_outfile, p_config):
+    for i in range(len(p_line)):
+        if i > 0:
+            p_outfile.writerow((p_config['county'], 
+                                p_config['office'],
+                                p_config['district'],
+                                p_partyList[i-1], 
+                                p_candidateList[i-1], 
+                                clean_text_values(p_line[i]) 
+                               ))
+    return
+
 def process_header_line( p_candidateList, p_partyList, p_line):
-    working = p_line.replace('\r', '|')
-    for header in working.split(","):
+    for header in p_line:
         populate_candidate_party_lists(p_candidateList, p_partyList, header)
     return
 
-def process_data_line():
+def process_data_line( p_candidateList, p_partyList, p_line, p_outfile, p_config):
+    if "TOTAL" in p_line[0]:
+        print_county_totals(p_candidateList, p_partyList, p_line, p_outfile, p_config)
     return
 
 def process_single_file(p_config, p_outfile, p_infile):
@@ -77,13 +92,14 @@ def process_single_file(p_config, p_outfile, p_infile):
     if os.path.isfile(p_infile) != True:
         print 'ERROR: Input File ' + p_infile + ' does not exist'
     else:
-        with open(p_infile) as file_reader:
-            for line in file_reader:
+        with open(p_infile, 'rb') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for line in csvreader:
                 counter = counter + 1
                 if counter == 1:
                     process_header_line(candidateList, partyList, line)
                 else:
-                    process_data_line
+                    process_data_line(candidateList, partyList, line, p_outfile, p_config)
     return
 
 def process_input_files(p_config, p_outfile, p_config_key):
@@ -115,5 +131,4 @@ try:
     process_config_data(config, out_file)
 except:
     print sys.exc_info()[0]
-    out_file.close()
 
