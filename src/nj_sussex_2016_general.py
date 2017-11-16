0ffd3dae3a8c1f8b6518d56ae184505ab980a2a2
cholq <cdholquist@gmail.com>
2017-11-15 22:27:18 -0500

    creating object tree with county, precinct and office data

--- src/nj_sussex_2016_general.py
+++ src/nj_sussex_2016_general.py
@@ -30,18 +30,47 @@ import re
 
 
 class LineType:
-    TOWN = 1
+    PRECINCT = 1
     RACE = 2
     VOTEFOR = 3
     CANDIDATE = 4
     OTHER = 10
 
+class County(object):
+    def __init__ (self, county):
+        self.county = county 
+        self.precincts = []
+
+    def writeToCSV(self, outFile):
+        for p in self.precincts:
+            for o in p.offices: 
+                for r in o.results:
+                    outFile.writerow( (self.county, p.precinct, o.office, '', r.candidate, r.party, r.votes) )
+        return
+
+
+class Precinct(object):
+    def __init__ (self, precinct):
+        self.precinct = precinct 
+        self.offices = []
+
+class Office(object):
+    def __init__ (self, office):
+        self.name = office
+        self.results = []
+
+class CandidateResults(object):
+    def __init__ (self, candidate, party, votes):
+        self.candidate = ''
+        self.party = ''
+        self.votes = 0
+
 class ContestRecord (object):
 
-    def __init__ (self, xmlKey, county, office, district):
+    def __init__ (self, county, office, municipality, district):
         self.county = county
-        self.xmlKey = xmlKey
         self.office = office
+        self.municipality = municipality
         self.district = district
         self.party = ''
         self.precinct = ''
@@ -64,8 +93,8 @@ def openOutputFile(outputPath):
     return writer
 
 def printLine(current_line, line_type):
-    if line_type == LineType.TOWN:
-        print "TOWN    :" + current_line
+    if line_type == LineType.PRECINCT:
+        print "PRECINCT    :" + current_line
     elif line_type == LineType.RACE:
         print "RACE    :" + current_line
     elif line_type == LineType.VOTEFOR:
@@ -105,10 +134,10 @@ def isCandidateLine(current_line):
             returnValue = True
     return returnValue
 
-def determineLineType(current_line, previous_value):
+def determineLineType(current_line):
     returnType = LineType.OTHER
     if isTownLine(current_line):
-        returnType = LineType.TOWN
+        returnType = LineType.PRECINCT
     elif isVoteForLine(current_line):
         returnType = LineType.VOTEFOR
     elif isRaceLine(current_line):
@@ -117,17 +146,58 @@ def determineLineType(current_line, previous_value):
         returnType = LineType.CANDIDATE
     return returnType
 
+def inPreTag(current_line, current_value):
+    returnValue = current_value
+    if "<pre>" in current_line:
+        returnValue = True
+    if "</pre>" in current_line:
+        returnValue = False
+    return returnValue
+
+def extractPrecinctName(current_line):
+    pName = ""
+    pIndex = 0
+    for w in current_line.split(' '):
+        if pIndex > 0:
+            pName += w + " "
+        pIndex += 1
+    pName = pName.rstrip()
+    return pName
+
+def extractRaceName(current_line):
+    rName = ""
+    if "PRESIDENT" in current_line or "HOUSE OF REPRESENTATIVES" in current_line:
+        rName = current_line.rstrip()
+    return rName
+
+def processLine(current_line, countyObj):
+    line_type = determineLineType(current_line) 
+    if line_type == LineType.PRECINCT:
+        #print "PRECINCT:" + current_line
+        precinctObj = Precinct(extractPrecinctName(current_line))
+        countyObj.precincts.append(precinctObj)
+    elif line_type == LineType.RACE:
+        print "RACE    :" + current_line
+        race_name = extractRaceName(current_line)
+        if len(race_name) > 0:
+            officeObj = Office(race_name)
+            pLen = len(countyObj.precincts)
+            countyObj.precincts[pLen - 1].offices.append(officeObj)
+    #elif line_type == LineType.VOTEFOR:
+    #    print "VOTEFOR :" + current_line
+    #elif line_type == LineType.CANDIDATE:
+    #    print "CAND    :" + current_line
+    #elif line_type == LineType.OTHER:
+    #    print "OTHER   :" + current_line
+
 def stepThruData():
-    print_data = False
+    use_line = False
     line_type = LineType.OTHER
+    countyObj = County('Sussex')
     for line in open('../../openelections-sources-nj/2016/Sussex/GENERAL-EL30-OFFICIAL.html'):
-        if "<pre>" in line:
-            print_data = True
-        if "</pre>" in line:
-            print_data = False
-        if print_data:
-            line_type = determineLineType(line, line_type) 
-            #printLine(line, line_type)
+        use_line = inPreTag(line, use_line)
+        if use_line:
+            processLine(line, countyObj)
 
 try:
     arg_parser = argparse.ArgumentParser(description='Parse New Jersey Sussex County data.')
